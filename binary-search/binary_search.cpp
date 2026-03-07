bb96b77f87ab6bd8c0f216dd1c4a830fa4ff223d
ARPIT SINGH <168482122+Arpit2singh@users.noreply.github.com>
2026-03-07 17:37:08 +0530

    ✅ binary-search - O(log N) - Auto Upload

---
+++ binary-search/binary_search.cpp
@@ -0,0 +1,6 @@
+// Problem: binary-search
+// Time Complexity: O(log N)
+// Space Complexity: O(1)
+// Auto uploaded via n8n 🚀
+
+class Solution {public:    int search(vector<int>& nums, int target) {                int low = 0 ;         int high = nums.size()-1 ;        while(low <= high){            int mid = low + (high-low)/2 ;             if(nums[mid] == target){                return mid  ;            }            else if(nums[mid] > target){                high = mid -1 ;            }            else{                low = mid +1 ;
