2b759b360bd81d632584c56efed3f97d6c339f0c
ARPIT SINGH <168482122+Arpit2singh@users.noreply.github.com>
2026-03-09 01:51:15 +0530

    ✅ two-sum - O(N) - Auto Upload

---
+++ two-sum/solution.cpp
@@ -0,0 +1,6 @@
+// Problem: two-sum
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+// Auto uploaded via n8n 🚀
+
+class Solution {public:    vector<int> twoSum(vector<int>& nums, int target) {        unordered_map<int,int>mp ; 
