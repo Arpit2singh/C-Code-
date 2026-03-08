8f852d77bd2d33bfb4de9345e847b9a19e29e03c
ARPIT SINGH <168482122+Arpit2singh@users.noreply.github.com>
2026-03-09 01:57:54 +0530

    ✅ binary-search - O(N) - Auto Upload

---
+++ binary-search/vector_search.cpp
@@ -0,0 +1,6 @@
+// Problem: binary-search
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+// Auto uploaded via n8n 🚀
+
+class Solution {public:    int search(vector<int>& nums, int target) {        
